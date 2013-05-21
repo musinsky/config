27c53d0dc43e85b6642c638f7c47805ca2bd1792
Jan Musinsky <musinsky@gmail.com>
2013-05-21 17:06:33 +0200

    move GistHub from https://gist.github.com/musinsky/2370651

---
+++ GistHub/GistHub.php
@@ -0,0 +1,41 @@
+<?php
+if( !defined( 'MEDIAWIKI' ) ) {
+  echo( "This file is part of an extension to the MediaWiki software and cannot be used standalone.\n" );
+  die( 1 );
+}
+
+$wgExtensionCredits['parserhook'][] = array(
+                                            'name' =>        'GistHub',
+                                            'author' =>      'Adam Meyer, Jan Musinsky',
+                                            'description' => 'Import file(s) from [https://gist.github.com/ github:gist]',
+                                            'url' =>         'https://github.com/musinsky/config/tree/master/GistHub'
+                                            );
+
+if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
+  $wgHooks['ParserFirstCallInit'][] = 'efGistHub';
+} else {
+  $wgExtensionFunctions[] = 'efGistHub';
+}
+
+function efGistHub() {
+  global $wgParser;
+  $wgParser->setHook( 'gisthub', 'efGistHubRender' );
+  return true;
+}
+
+function efGistHubRender( $input, $args, $parser ) {
+  $attr = array();
+  // This time, make a list of attributes and their values,
+  // and dump them, along with the user input
+  foreach( $args as $name => $value ) {
+    if ( $name == 'gist' ) {
+      $num = $value;
+    }
+    if ( $name == 'file' ) {
+      $fname = $value;
+    }
+  }
+
+  return '<script src="https://gist.github.com/'.$num.'.js?file='.$fname.'"></script>';
+}
+?>
---
+++ GistHub/README.md
@@ -0,0 +1,30 @@
+GistHub
+-------
+GistHub is simple [MediaWiki](https://www.mediawiki.org) extension that allows you to embed [GitHub Gist](https://gist.github.com) file(s)
+
+### Installation
+
+* download this file
+```
+wget https://raw.github.com/musinsky/config/master/GistHub/GistHub.php
+```
+
+* and place it in ``$IP/extensions/GistHub/GistHub.php``
+
+* add the following code to ``LocalSettings.php``
+```
+require_once("$IP/extensions/GistHub/GistHub.php");
+```
+
+### Usage
+* import file (or all files) from GitHub Gist
+```
+<gisthub gist="2370651"/>
+```
+
+* import only one specif file (for example ``GistHub.wiki``) from GitHub Gist
+```
+<gisthub gist="2370651" file="GistHub.wiki"/>
+```
+
+* use ``/`` at the end of line is important
