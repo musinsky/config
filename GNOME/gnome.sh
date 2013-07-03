83c93e5d4542d4602e604ae85c543a5707808fd0
Jan Musinsky <musinsky@gmail.com>
2013-07-03 23:29:52 +0400

    update to GNOME 3.8

--- GNOME/gnome.sh
+++ GNOME/gnome.sh
@@ -1,10 +1,10 @@
-# GNOME 3.6.2
+# GNOME 3.8.2
 
 gsettings set org.gnome.desktop.default-applications.terminal exec 'gnome-terminal --geometry=96x24+0-0'
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-x'
 
-gsettings set org.gnome.desktop.background picture-options 'none'
 gsettings set org.gnome.desktop.background color-shading-type 'vertical'
+gsettings set org.gnome.desktop.background picture-options 'none'
 gsettings set org.gnome.desktop.background primary-color '#3C78B4'          # (Red: 60 Green: 120 Blue: 180)
 gsettings set org.gnome.desktop.background secondary-color '#1E3C5A'        # (Red: 30 Green:  60 Blue:  90)
 gsettings set org.gnome.desktop.background show-desktop-icons true
@@ -12,30 +12,33 @@ gsettings set org.gnome.desktop.background show-desktop-icons true
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'ru+phonetic'), ('xkb', 'sk+qwerty')]"
 
 gsettings set org.gnome.desktop.interface buttons-have-icons true
-gsettings set org.gnome.desktop.interface menus-have-icons true
-gsettings set org.gnome.desktop.interface font-name 'Cantarell 9'                    # Cantarell 11
-gsettings set org.gnome.desktop.interface document-font-name 'Sans 9'                # Sans 11
-gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 9'          # Monospace 11
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface document-font-name 'Sans 9'                # Sans 11
+gsettings set org.gnome.desktop.interface font-name 'Cantarell 9'                    # Cantarell 11
 gsettings set org.gnome.desktop.interface menubar-accel ''                           # disable F10
+gsettings set org.gnome.desktop.interface menus-have-icons true
+gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 9'          # Monospace 11
 
 gsettings set org.gnome.desktop.screensaver lock-enabled false                       # disable lock screen
+
 gsettings set org.gnome.desktop.session idle-delay 3000                              # turn screen off
 
-gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]"                  # disable Alt+F1
 gsettings set org.gnome.desktop.wm.keybindings close "[]"                            # disable Alt+F4
+gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]"                  # disable Alt+F1
 gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>R']"         # replace Alt+F2 by Alt+R
+gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Ctrl><Alt>M']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Alt>F1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Alt>F2']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Alt>F3']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Alt>F4']"
-gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Ctrl><Alt>M']"
 
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 9'      # Cantarell Bold 11
 gsettings set org.gnome.desktop.wm.preferences visual-bell true
 gsettings set org.gnome.desktop.wm.preferences visual-bell-type 'frame-flash'
+
+gsettings set org.gnome.Evince.Default sizing-mode 'fit-page'
 
 gsettings set org.gnome.nautilus.desktop home-icon-visible false
 gsettings set org.gnome.nautilus.desktop network-icon-visible false
@@ -43,19 +46,20 @@ gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-gsettings set org.gnome.nautilus.preferences show-image-thumbnails 'local-only'
 gsettings set org.gnome.nautilus.preferences show-directory-item-counts 'local-only'
-
-gsettings set org.gnome.settings-daemon.peripherals.keyboard input-sources-switcher 'shift-l-shift-r'
+gsettings set org.gnome.nautilus.preferences show-image-thumbnails 'local-only'
 
 gsettings set org.gnome.settings-daemon.plugins.power button-power 'shutdown'        # interactive
-gsettings set org.gnome.settings-daemon.plugins.power sleep-display-ac 1800
-gsettings set org.gnome.settings-daemon.plugins.power sleep-display-battery 600
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 1800
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 600
 
 gsettings set org.gnome.shell always-show-log-out true                               # 'Log Out' in the user menu
+
 gsettings set org.gnome.shell.calendar show-weekdate true
+
 gsettings set org.gnome.shell.overrides button-layout ':minimize,maximize,close'
 gsettings set org.gnome.shell.overrides dynamic-workspaces false
+
 
 
 gconftool-2 --type string --set /apps/gnome-terminal/keybindings/help "F12"          # replace F1 to F12
