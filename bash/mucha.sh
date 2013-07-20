e8634e9b47b67ec9e9a31cbbe2963d9e559476d9
Jan Musinsky <musinsky@gmail.com>
2013-07-20 14:17:39 +0200

    Update mucha.sh

--- bash/mucha.sh
+++ bash/mucha.sh
@@ -4,6 +4,8 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib
 export MANPATH=:$MANPATH:$ROOTSYS/man
 alias root='root -l'
 
-export PATH=/opt/texlive/2012/bin/x86_64-linux:$PATH
-export MANPATH=/opt/texlive/2012/texmf/doc/man:$MANPATH
-export INFOPATH=/opt/texlive/2012/texmf/doc/info:$INFOPATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cern/xrootd/lib64
+
+export PATH=/opt/texlive/2013/bin/x86_64-linux:$PATH
+export MANPATH=/opt/texlive/2013/texmf-dist/doc/man:$MANPATH
+export INFOPATH=/opt/texlive/2013/texmf-dist/doc/info:$INFOPATH
