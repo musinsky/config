084ffe7cfc48a0271a9825dc74682d42ac033b30
Jan Musinsky <musinsky@gmail.com>
2013-08-09 19:32:21 +0200

    add dump to XML format

--- MediaWiki/wiki-backup.sh
+++ MediaWiki/wiki-backup.sh
@@ -3,14 +3,21 @@
 
 DBname=alice_wiki
 DBuser=wikiuser
-MWSQL=$DBname-dump_`date +%F`.sql
 MWDIR=/opt/mediawiki
-MWBCP=wiki-alice-backup_`date +%F`.tar.gz
+DUMP="$DBname"_dump_`date +%F`
+MWXML=$DUMP.xml
+MWSQL=$DUMP.sql
+MWBCP="$DBname"_backup_`date +%F`.tar.gz
 
+# XML
+php $MWDIR/maintenance/dumpBackup.php --current > $MWXML
+
+# SQL
 echo "mysqldump --user=$DBuser --password $DBname"
 mysqldump --user=$DBuser --password $DBname -c > $MWSQL
-tar -czf $MWBCP $MWSQL -C $MWDIR LocalSettings.php images \
+
+tar -czf $MWBCP $MWXML $MWSQL -C $MWDIR LocalSettings.php images \
     -P /etc/httpd/conf.d/mediawiki.conf \
     /var/www/html/robots.txt /var/www/html/google*.html
-rm $MWSQL
+rm $MWXML $MWSQL
 chmod 400 $MWBCP
