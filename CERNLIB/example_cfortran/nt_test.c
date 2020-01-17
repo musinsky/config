3a818bfd04b0c1fe7ada9b0c2d7e5f3af43ce081
Jan Musinsky <musinsky@gmail.com>
2020-01-17 14:30:21 +0100

    move nt_test.c to cfortran dir

--- CERNLIB/example_cfortran/nt_test.c
+++ CERNLIB/example_cfortran/nt_test.c
@@ -5,13 +5,13 @@
  * routines.
  *
  * see
- * http://osksn2.hep.sci.osaka-u.ac.jp/~taku/doc/hbook.pdf
- * http://www2.pv.infn.it/~sc/cern/paw.pdf
- * for some of the last surviving documentation regarding HBOOK calls and
- * running PAW that I can find.
+ * https://cds.cern.ch/record/2296378/files/hbook.pdf
+ * https://cds.cern.ch/record/2296392/files/paw.pdf
+ * for some of the last surviving documentation regarding HBOOK calls
+ * and running PAW that I can find.
  *
  *
- * successfully compiled and ran on Fedora release 30 X86_64
+ * successfully compiled and ran on Fedora release 30 x86_64
  * gcc -c -DgFortran nt_test.c -I/usr/include/cernlib/2006
  * gfortran -o nt_test nt_test.o `cernlib packlib`
  *
@@ -30,7 +30,7 @@
 #include <cfortran.h>
 #include <packlib.h>
 
-#define PAWC_SIZE 20000
+#define PAWC_SIZE 50000
 
 typedef struct { float PAW[PAWC_SIZE]; } PAWC_DEF;
 #define PAWC COMMON_BLOCK(PAWC,pawc)
@@ -66,11 +66,11 @@ int main(int argc, char **argv)
    * easily modified to read in data from a text file. */
   int i;
   for (i = 0; i < 10; ++i)
-  {
-    Data.x = i;
-    Data.y = i*2;
-    HFNT(10);
-  }
+    {
+      Data.x = i;
+      Data.y = i*2;
+      HFNT(10);
+    }
   HROUT(10, icycle, " ");
 
   HREND("TEST");
