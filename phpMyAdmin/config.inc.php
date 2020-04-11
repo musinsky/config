ae80d374dcfed727f64f91991c801b7ed56d9e9b
Jan Musinsky <musinsky@gmail.com>
2020-04-11 03:07:51 +0300

    update phpMyAdmin config file

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -3,20 +3,19 @@
  * https://github.com/musinsky/config/tree/master/phpMyAdmin
  */
 
+declare(strict_types=1);
+
 /* Servers configuration */
 $i = 0;
 
 /* Server: localhost [1] */
 $i++;
-$cfg['Servers'][$i]['host'] = 'localhost';
-$cfg['Servers'][$i]['connect_type'] = 'tcp';
-$cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['auth_type'] = 'http';
+$cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 /* End of servers configuration */
 
-$cfg['DefaultLang'] = 'en-utf-8';
 $cfg['MaxRows'] = '999';
 
 ?>
