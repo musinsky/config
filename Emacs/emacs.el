fd0d56103948fb83a7c1a93fc97483d2aa743dc0
Jan Musinsky <musinsky@gmail.com>
2020-05-29 23:47:32 +0300

    [emacs] fill paragraph

--- Emacs/emacs.el
+++ Emacs/emacs.el
@@ -100,9 +100,11 @@
 (prefer-coding-system 'utf-8)
 
 ;; https://www.emacswiki.org/emacs/LineWrap
+;; https://www.emacswiki.org/emacs/FillParagraph
 (setq-default fill-column 80) ; M-x set-fill-column
 ;; M-q (bound) M-x fill-paragraph
 ;; M-x set-fill-column RET 9999 (or 0) RET M-q
+;; C-u M-q => block fill paragraph (M-1 M-q faster to type)
 ;;(setq-default auto-fill-function 'do-auto-fill) ; M-x auto-fill-mode
 ;; https://www.emacswiki.org/emacs/VisualLineMode
 ;;(global-visual-line-mode t) ; M-x visual-line-mode
