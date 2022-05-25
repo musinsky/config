6f3d01d0aa8423288bf220aa3f55e13ccc0b36b3
Jan Musinsky <musinsky@gmail.com>
2022-05-25 17:30:30 +0200

    [MediaWiki] mariadb replace mysql plus add dumpUploads

--- MediaWiki/wiki-backup.sh
+++ MediaWiki/wiki-backup.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/bash
 
-# 2022-05-05
+# 2022-05-25
 # https://github.com/musinsky/config/blob/master/MediaWiki/wiki-backup.sh
 
-DBname=$(hostname)_wiki
+DBname=$(hostname)_wiki   # better choice is parsing $wgDBname in LocalSettings.php
 DBuser=wikiuser
 MWDIR=/opt/mediawiki
-DUMP="$DBname"_dump_$(date +%F)
+DUMP="$DBname"-dump_$(date +%F)
 MWXML=$DUMP.xml
 MWSQL=$DUMP.sql
+MWUPL=upload.files
 MWEXT=extensions.list
-MWBCP="$DBname"_backup_$(date +%F).tar.gz
+MWBCP="$DBname"-backup_$(date +%F).tar.gz
 BCPSC=$(basename $0)
 
-# XML
-php $MWDIR/maintenance/dumpBackup.php --current > $MWXML
-
 # SQL
-echo "mysqldump --user=$DBuser --password $DBname"
-mysqldump --user=$DBuser --password $DBname -c > $MWSQL
+echo "mariadb-dump --user=$DBuser --password $DBname"
+mariadb-dump --user=$DBuser --password $DBname > $MWSQL
+
+# XML and upload files
+php $MWDIR/maintenance/dumpBackup.php --current > $MWXML
+php $MWDIR/maintenance/dumpUploads.php > $MWUPL
 
 # extensions list
 ls -l $MWDIR/extensions | grep '^d' > $MWEXT
 
 # archiving
-tar -czf $MWBCP $MWXML $MWSQL $MWEXT $BCPSC -C $MWDIR LocalSettings.php images \
+tar -czf $MWBCP $MWSQL $MWXML $MWUPL $MWEXT $BCPSC -C $MWDIR LocalSettings.php images \
     -P /etc/httpd/conf.d/mediawiki.conf \
     /var/www/html/robots.txt /var/www/html/google*.html
-rm $MWXML $MWSQL $MWEXT
+rm $MWSQL $MWXML $MWUPL $MWEXT
 chmod 400 $MWBCP
