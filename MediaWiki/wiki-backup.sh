eaa260b724ebc4b68209d9cc9cd05198d24c55e7
Jan Musinsky <musinsky@gmail.com>
2022-06-06 16:35:53 +0200

    [MediaWiki] backup script now parsing LocalSettings.php

--- MediaWiki/wiki-backup.sh
+++ MediaWiki/wiki-backup.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/bash
 
-# 2022-05-25
+# 2022-06-06
 # https://github.com/musinsky/config/blob/master/MediaWiki/wiki-backup.sh
 
-DBname=$(hostname)_wiki   # better choice is parsing $wgDBname in LocalSettings.php
-DBuser=wikiuser
 MWDIR=/opt/mediawiki
+MWLSF=LocalSettings.php
+DBname=$(grep DBname $MWDIR/$MWLSF | grep -oP '(?<=").*(?=")')
+DBuser=$(grep DBuser $MWDIR/$MWLSF | grep -oP '(?<=").*(?=")')
+# DBcharset=$(grep DBTableOptions $MWDIR/$MWLSF | grep -oP '(?<=DEFAULT CHARSET=).*(?=")')
+# mysqldump --default-character-set=binary
 DUMP="$DBname"-dump_$(date +%F)
-MWXML=$DUMP.xml
 MWSQL=$DUMP.sql
-MWUPL=upload.files
-MWEXT=extensions.list
-MWBCP="$DBname"-backup_$(date +%F).tar.gz
+MWXML=$DUMP.xml
+MWUPL=$DUMP.uploads
+MWEXT=extensions.dir
+MWBCP="$DBname"-backup_$(date +%F).tar.xz
 BCPSC=$(basename $0)
 
 # SQL
-echo "mariadb-dump --user=$DBuser --password $DBname"
-mariadb-dump --user=$DBuser --password $DBname > $MWSQL
+echo "mysqldump --user=$DBuser --password $DBname"
+mysqldump --user=$DBuser --password $DBname > $MWSQL
 
-# XML and upload files
+# XML and uploaded files
 php $MWDIR/maintenance/dumpBackup.php --current > $MWXML
 php $MWDIR/maintenance/dumpUploads.php > $MWUPL
 
-# extensions list
+# extensions dir
 ls -l $MWDIR/extensions | grep '^d' > $MWEXT
 
 # archiving
-tar -czf $MWBCP $MWSQL $MWXML $MWUPL $MWEXT $BCPSC -C $MWDIR LocalSettings.php images \
+tar -cJf $MWBCP $MWSQL $MWXML $MWUPL $MWEXT $BCPSC -C $MWDIR $MWLSF images \
     -P /etc/httpd/conf.d/mediawiki.conf \
     /var/www/html/robots.txt /var/www/html/google*.html
 rm $MWSQL $MWXML $MWUPL $MWEXT
