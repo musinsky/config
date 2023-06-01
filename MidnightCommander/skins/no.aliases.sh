6aeb0adecb0d661fac8aaca9c258e9d6b57af8ac
Jan Musinsky <musinsky@gmail.com>
2023-06-01 13:32:18 +0200

    [MidnightCommander] more sed info

--- MidnightCommander/skins/no.aliases.sh
+++ MidnightCommander/skins/no.aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 2023-05-31
+# 2023-06-01
 
 F_ALIAS='default-gray256.ini'
 F_NOALIAS='default-gray256.no.aliases.ini'
@@ -22,9 +22,29 @@ sed -i '/^\[aliases\]/d' "$F_NOALIAS"
 # replace (all) $F_ALIAS by $F_NOALIAS (https github file name)
 sed -i "s/$F_ALIAS/$F_NOALIAS/g" "$F_NOALIAS"
 
-# replace all color names after line starts with '[core]' ('gray' is in title)
+# replace all color names between line starts with '[core]'
+# and the last line '$' ('gray' is in the title and not as a color name)
 # be careful with order (lightgray/gray and brightblue/blue)
-L_NUMBER="$(grep -n '\[core\]' "$F_NOALIAS" | cut -f 1 -d ':')"
+L_NUMBER="$(grep -n '^\[core\]' "$F_NOALIAS" | cut -f 1 -d ':')"
 sed -i "$L_NUMBER,$ {s/lightgray/color250/g;  s/gray/color254/g}" "$F_NOALIAS"
 sed -i "$L_NUMBER,$ {s/brightblue/color244/g; s/blue/color238/g}" "$F_NOALIAS"
 sed -i "$L_NUMBER,$  s/cyan/color244/g"                           "$F_NOALIAS"
+# or to same (without variable $L_NUMBER)
+# sed -i '/^\[core\]/,$ {s/lightgray/color250/g;  s/gray/color254/g}' "$F_NOALIAS"
+
+
+# address range (selecting lines by text matching)
+# https://www.gnu.org/software/sed/manual/html_node/sed-addresses.html
+# https://www.gnu.org/software/sed/manual/html_node/Regexp-Addresses.html
+#
+# print 'p' lines between line starts with '[' and line starts with '['
+# sed -n '/^\[/,/^\[/ p' "$F_ALIAS"
+# print 'p' lines between line with 'description' and line with 'Lines'
+# sed -n '/description/,/Lines/ p' "$F_ALIAS"
+#
+# print 'p' lines between line starts with '[core]' and line starts with '['
+# sed -n '/^\[core\]/,/^\[/ p' "$F_ALIAS"
+# print 'p' lines between line starts with '[core]' and last line '$'
+# sed -n '/^\[core\]/,$ p' "$F_ALIAS"
+# print 'p' lines between first line '0' and line starts with '[core]'
+# sed -n '0,/^\[core\]/ p' "$F_ALIAS"
