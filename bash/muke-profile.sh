07bff8f3b39c1f70683cc448266222a7c84dd2ba
Jan Musinsky <musinsky@gmail.com>
2023-06-05 20:44:34 +0200

    [bash] reserved/global function begin with underscore

--- bash/muke-profile.sh
+++ bash/muke-profile.sh
@@ -1,7 +1,7 @@
 # 2023-06-05
 # https://github.com/musinsky/config/blob/master/bash/muke-profile.sh
 
-function muke_var_value {
+function __muke_var_value {
     local var_value="${!1}"
     # printf "var_name         = %s\n" "$1"
     # printf "var_value        = %s\n" "$var_value"
@@ -22,19 +22,19 @@ function muke_var_value {
 # https://sft.its.cern.ch/jira/browse/ROOT-9309
 # it's better to never set ROOTSYS, LD_LIBRARY_PATH and never source thisroot.sh
 #   ROOTSYS='/cern/root'
-#   muke_var_value PATH "$ROOTSYS/bin"
-#   muke_var_value LD_LIBRARY_PATH "$ROOTSYS/lib"
-#   muke_var_value CMAKE_PREFIX_PATH "$ROOTSYS"
+#   __muke_var_value PATH "$ROOTSYS/bin"
+#   __muke_var_value LD_LIBRARY_PATH "$ROOTSYS/lib"
+#   __muke_var_value CMAKE_PREFIX_PATH "$ROOTSYS"
 
 ROOT_PATH='/cern/root'
-muke_var_value PATH         "$ROOT_PATH/bin"
-muke_var_value PYTHONPATH   "$ROOT_PATH/lib"
-muke_var_value JUPYTER_PATH "$ROOT_PATH/etc/notebook"
+__muke_var_value PATH         "$ROOT_PATH/bin"
+__muke_var_value PYTHONPATH   "$ROOT_PATH/lib"
+__muke_var_value JUPYTER_PATH "$ROOT_PATH/etc/notebook"
 
-muke_var_value PATH            '/cern/xrootd/bin'
-muke_var_value LD_LIBRARY_PATH '/cern/xrootd/lib64'
+__muke_var_value PATH            '/cern/xrootd/bin'
+__muke_var_value LD_LIBRARY_PATH '/cern/xrootd/lib64'
 
-muke_var_value PATH '/opt/texlive/2023/bin/x86_64-linux'
+__muke_var_value PATH '/opt/texlive/2023/bin/x86_64-linux'
 
 # MAN and INFO are added automatically
 export PATH LD_LIBRARY_PATH PYTHONPATH JUPYTER_PATH
