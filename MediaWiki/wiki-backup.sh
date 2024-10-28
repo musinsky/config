86508b88d1df4e0001a06adfc4d60c95f172fbb3
Jan Musinsky <musinsky@gmail.com>
2024-10-28 19:08:51 +0100

    [MediaWiki] add files

--- MediaWiki/wiki-backup.sh
+++ MediaWiki/wiki-backup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# 2024-06-26
+# 2024-10-28
 # https://github.com/musinsky/config/blob/master/MediaWiki/wiki-backup.sh
 
-[[ $EUID -ne 0 ]] && { printf "only root user\n"; exit 1; }
+[ "$EUID" -ne 0 ] && { printf "only root user\n"; exit 1; }
 
 MWDIR="/opt/mediawiki"
 MWLSF="LocalSettings.php"
@@ -18,37 +18,39 @@ DUMP="$DBname-dump_$(date +%F)"
 MWSQL="$DUMP.sql"
 MWXML="$DUMP.xml"
 MWUPL="$DUMP.uploads"
+FILES="$DUMP.files"
 MWEXT="$DUMP.extensions"
 
 # SQL
 printf "mysqldump --user=%s --password  %s" "$DBuser" "$DBname"
 printf "   # password='%s'\n" "$DBpass"
 mysqldump --user="$DBuser" --password "$DBname" > "$MWSQL"
-
-# XML and uploaded files
+# XML
 php "$MWDIR/maintenance/dumpBackup.php" --current > "$MWXML"
+# uploaded files (images)
 php "$MWDIR/maintenance/dumpUploads.php" > "$MWUPL"
-
-# extensions dir
-ls --time-style=long-iso -l -d "$MWDIR/extensions"/*/ > "$MWEXT" # don't quotes "*"
+# files (local files)
+ls -lR --group-directories-first --time-style="+%F %T" "$MWDIR/files" > "$FILES"
+# extensions (dir names)
+ls -ld --time-style="+%F %T" "$MWDIR/extensions"/*/ > "$MWEXT" # don't quotes "*"
 
 # archiving
-tar -chJf "$MWBCP" "$MWSQL" "$MWXML" "$MWUPL" "$MWEXT" \
-    --exclude="$MWDIR/images/thumb" \
-    "$MWDIR/$MWLSF" "$MWDIR/images" \
+tar -chJf "$MWBCP" \
+    "$MWSQL" "$MWXML" "$MWUPL" "$FILES" "$MWEXT" \
+    --exclude="$MWDIR/images/thumb" "$MWDIR/images" "$MWDIR/$MWLSF" \
     -C "$(dirname "$0")" "$(basename "$0")" \
     -P /etc/httpd/conf.d/mediawiki.conf \
     /var/www/html/robots.txt \
     /var/www/html/google*.html \
     && printf "'%s' created\n" "$MWBCP"
-rm "$MWSQL" "$MWXML" "$MWUPL" "$MWEXT"
+rm "$MWSQL" "$MWXML" "$MWUPL" "$FILES" "$MWEXT"
 chmod 400 "$MWBCP"
 
-### webfiles ###
-WFDIR="/opt/webfiles"
-[[ -d "$WFDIR" ]] || { printf "'%s' no such directory\n" "$WFDIR"; exit 1; }
+# ### webfiles ###
+# WFDIR="/opt/webfiles"
+# [ -d "$WFDIR" ] || { printf "'%s' no such directory\n" "$WFDIR"; exit 1; }
 
-WFBCP="webfiles_$(date +%F).tar.gz"
-tar -chzf "$WFBCP" -P "$WFDIR" /etc/httpd/conf.d/webfiles.conf \
-    && printf "'%s' created\n" "$WFBCP"
-chmod 444 "$WFBCP"
+# WFBCP="webfiles_$(date +%F).tar.gz"
+# tar -chzf "$WFBCP" -P "$WFDIR" /etc/httpd/conf.d/webfiles.conf \
+    #     && printf "'%s' created\n" "$WFBCP"
+# chmod 444 "$WFBCP"
