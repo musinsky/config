14e661d2d29aa9111465f6c42d63e5abed1fa9d6
Jan Musinsky <musinsky@gmail.com>
2025-01-31 15:40:32 +0100

    [freedesktop] fonts.get.ms.sh update 2025-01-31 15:40:32

--- freedesktop/fonts.get.ms.sh
+++ freedesktop/fonts.get.ms.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 2025-01-29
+# 2025-01-31
 
 copy_font_file() {
     local ff_src_full="$1" # [source] font file (full dir/file)
@@ -23,21 +23,21 @@ copy_font_file() {
         ff_dst_file="${ff_src_full##*/}" # basename in bash
     fi
 
-    ff_dst_file="$ff_dst_dir/$ff_dst_file" # full path
+    ff_dst_file="$ff_dst_dir/$ff_dst_file" # full dir/file
     #printf "source      font file: '%s'\n" "$ff_src_full"
     #printf "destination font file: '%s'\n" "$ff_dst_file"
     cp -p "$ff_src_full" "$ff_dst_file" && chmod 644 "$ff_dst_file"
 }
 find_font_files() {
-    local find_dir="$1"     # [source] find starting point
+    local find_dir="$1"     # [source] find starting point (font dir)
     local find_pattern="$2" # [source] find name pattern (font files)
     local ff_dir="$3"       # [destination] font dir
 
     # find "$find_dir" -type f -name "$find_pattern" -exec printf "ff: '%s'\n" '{}' \;
     find "$find_dir" -type f -name "$find_pattern" -print0 |
-        while IFS= read -r -d '' f_line; do   # -d $'\0'
-            #printf "ff: '%s'\n" "$f_line"
-            copy_font_file "$f_line" "$ff_dir"
+        while IFS= read -r -d '' f_file; do   # -d $'\0'
+            #printf "ff: '%s'\n" "$f_file"
+            copy_font_file "$f_file" "$ff_dir"
         done
 }
 
@@ -87,14 +87,12 @@ FONTS_DIR="$LINUX_FONTS_DIR/ms-comic-sans-fonts"
 find_font_files "$MS_FONTS_DIR" "comic*.ttf" "$FONTS_DIR" # 4 files
 fonts_summary "$FONTS_DIR"
 
-fc-cache && printf "fc-cache successfully done\n" 1>&2
-
 ########## MS Aptos Fonts ##########
 # https://learn.microsoft.com/en-us/typography/font-list/aptos
 # https://www.microsoft.com/en-us/download/details.aspx?id=106087
 # file 'Microsoft Aptos Fonts.zip' (ver: 4.40, date: 6/4/2024, 2979784 bytes)
 
-APTOS_ZIP="$HOME/Downloads/Microsoft Aptos Fonts.zip"
+APTOS_ZIP="$HOME/Downloads/Microsoft Aptos Fonts.zip" # download in browser
 [ -r "$APTOS_ZIP" ] || { printf "cannot read file '%s'\n" "$APTOS_ZIP" 1>&2; exit 1; }
 TMP_DIR="$(mktemp -d)" || exit 1
 unzip -q "$APTOS_ZIP" -d "$TMP_DIR" || exit 1
@@ -106,26 +104,42 @@ rm -rf "$TMP_DIR"
 
 ########## Google Fonts ##########
 # https://fonts.google.com/share?selection.family=Caveat|Caveat+Brush|Exo+2|Shantell+Sans
-# https://fonts.google.com/selection?family=Caveat|Caveat+Brush|Exo+2|Shantell+Sans
 # https://fonts.google.com/download?family=Caveat|Caveat+Brush|Exo+2|Shantell+Sans
 #
-# Google changed rules (2024-03), https://github.com/google/fonts/issues/7481
 # $ wget -O Caveat.zip https://fonts.google.com/download?family=Caveat # will not work
+# Google changed rules (2024-03), https://github.com/google/fonts/issues/7481
 
-GFONTS_ZIP="$HOME/Downloads/Caveat,Caveat_Brush,Exo_2,Shantell_Sans.zip"
+GFONTS_ZIP="$HOME/Downloads/Caveat,Caveat_Brush,Exo_2,Shantell_Sans.zip" # download in browser
 [ -r "$GFONTS_ZIP" ] || { printf "cannot read file '%s'\n" "$GFONTS_ZIP" 1>&2; exit 1; }
 TMP_DIR="$(mktemp -d)" || exit 1
 unzip -q "$GFONTS_ZIP" -d "$TMP_DIR" || exit 1
 
+# https://fonts.google.com/specimen/Caveat
 FONTS_DIR="$LINUX_FONTS_DIR/impallari-caveat-fonts"
 find_font_files "$TMP_DIR/Caveat/static" "*.ttf" "$FONTS_DIR" # 4 (static) files
 copy_font_file "$TMP_DIR/Caveat/Caveat-VariableFont_wght.ttf" "$FONTS_DIR" \
                "Caveat[wght].ttf" # 1 (variable) file
 fonts_summary "$FONTS_DIR"
-
+# https://fonts.google.com/specimen/Caveat+Brush
 FONTS_DIR="$LINUX_FONTS_DIR/impallari-caveat-brush-fonts"
 find_font_files "$TMP_DIR/Caveat_Brush" "*.ttf" "$FONTS_DIR" # 1 file
 fonts_summary "$FONTS_DIR"
+# https://fonts.google.com/specimen/Exo+2
+FONTS_DIR="$LINUX_FONTS_DIR/ndiscover-exo-2-fonts"
+find_font_files "$TMP_DIR/Exo_2/static" "*.ttf" "$FONTS_DIR" # 18 (static) files
+copy_font_file "$TMP_DIR/Exo_2/Exo2-VariableFont_wght.ttf" "$FONTS_DIR" \
+               "Exo2[wght].ttf"        # 1 (variable) file
+copy_font_file "$TMP_DIR/Exo_2/Exo2-Italic-VariableFont_wght.ttf" "$FONTS_DIR" \
+               "Exo2-Italic[wght].ttf" # 1 (variable) file
+fonts_summary "$FONTS_DIR"
+# https://fonts.google.com/specimen/Shantell+Sans
+FONTS_DIR="$LINUX_FONTS_DIR/arrowtype-shantell-sans-fonts"
+find_font_files "$TMP_DIR/Shantell_Sans/static" "*.ttf" "$FONTS_DIR" # 12 (static) files
+copy_font_file "$TMP_DIR/Shantell_Sans/ShantellSans-VariableFont_BNCE,INFM,SPAC,wght.ttf" \
+               "$FONTS_DIR" "ShantellSans[wght].ttf"        # 1 (variable) file
+copy_font_file "$TMP_DIR/Shantell_Sans/ShantellSans-Italic-VariableFont_BNCE,INFM,SPAC,wght.ttf" \
+               "$FONTS_DIR" "ShantellSans-Italic[wght].ttf" # 1 (variable) file
+fonts_summary "$FONTS_DIR"
+rm -rf "$TMP_DIR"
 
-#rm -rf "$TMP_DIR"
-#fc-cache && printf "fc-cache successfully done\n" 1>&2
+fc-cache && printf "fc-cache successfully done\n" 1>&2
