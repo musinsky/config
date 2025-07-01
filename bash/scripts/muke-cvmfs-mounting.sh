01428e5df8aa07a297526149df17c738a2d016ef
Jan Musinsky <musinsky@gmail.com>
2025-07-01 22:56:41 +0200

    [bash] parsing repos from cvmfs_config

--- bash/scripts/muke-cvmfs-mounting.sh
+++ bash/scripts/muke-cvmfs-mounting.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# 2025-06-20
+# 2025-07-01
 # https://github.com/musinsky/config/blob/master/bash/scripts/muke-cvmfs-mounting.sh
 
 print_status_usage() {
@@ -37,6 +37,12 @@ CVMFS_DIR="/cvmfs"
 CVMFS_REPOS=("cvmfs-config.cern.ch")
 CVMFS_REPOS+=("sft.cern.ch" "sft-nightlies.cern.ch")
 CVMFS_REPOS+=("alice.cern.ch" "alice-nightlies.cern.ch" "alice-ocdb.cern.ch")
+
+# # parsing 'cvmfs_config showconfig' command output is better (but slower) than
+# # parsing '/etc/cvmfs/default.local' file
+# IFS=',' read -r -a CVMFS_REPOS <<< "$(cvmfs_config showconfig -s none-repo |
+#     grep 'CVMFS_REPOSITORIES' | cut -d '=' -f 2 | cut -d ' ' -f 1)"
+# printf 'CVMFS_REPOSITORIES from cvmfs_config: %s\n' "${CVMFS_REPOS[@]}"
 
 [ "$#" -ne 1 ] && print_status_usage # just one argument
 [ "$(id -u)" -ne 0 ] && { printf 'root privileges required\n'; exit 1; }
